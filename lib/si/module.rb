7d4a00086771ec70fc4c15e6ca84a545d35fc2db
Simon Brook <simon@datanauts.co.uk>
2019-07-12 12:26:50 +0100

    adds decimal places as an option (instead of just length)
    Remove ruby 1.8 support

--- lib/si/module.rb
+++ lib/si/module.rb
@@ -13,51 +13,38 @@ module SI
       minus   = num < 0 ? '-' : ''
       nump    = num.abs
 
-      PREFIXES.keys.sort.reverse.select { |exp| (min_exp..max_exp).include? exp }.each do |exp|
-        denom = base ** exp
-        if nump >= denom || exp == min_exp
-          val = nump / denom
-          val = SI.round val, [length - val.to_i.to_s.length, 0].max
-          val = val.to_i if exp == 0 && num.is_a?(Fixnum)
-          val = val.to_s.ljust(length + 1, '0') if val.is_a?(Float)
+      PREFIXES.keys.sort.reverse.select { |exp| (min_exp..max_exp).cover? exp }.each do |exp|
+        denom = base**exp
+        next unless nump >= denom || exp == min_exp
+        val = nump / denom
+        dp = options[:decimal_places] || [length - val.to_i.to_s.length, 0].max
+        val = val.round dp
+        val = val.to_i if exp.zero? && num.is_a?(Integer)
+        val = val.to_s.ljust(dp, '0') if val.is_a?(Float)
 
-          return "#{minus}#{val}#{PREFIXES[exp]}"
-        end
+        return "#{minus}#{val}#{units[exp]}"
       end
 
       nil
     end
 
-    def revert str, options = {}
-      options = Hash[ DEFAULT.select { |k, v| k == :base } ].merge(options)
-      pair    = PREFIXES.to_a.find { |k, v| !v.empty? && str =~ /[0-9]#{v}$/ }
+    def revert(str, options = {})
+      options = { base: DEFAULT[:base] }.merge(options)
+      pair    = PREFIXES.to_a.find { |_k, v| !v.empty? && str =~ /[0-9]#{v}$/ }
 
       if pair
-        str[0...-1].to_f * (options[:base] ** pair.first)
+        str[0...-1].to_f * (options[:base]**pair.first)
       else
         str.to_f
       end
     end
-
-    if (RUBY_VERSION.split('.')[0, 2].map(&:to_i) <=> [1, 8]) == 1
-      def round val, ndigits
-        val.round ndigits
-      end
-    else
-      def round val, ndigits
-        exp = (10 ** ndigits).to_f
-        val = ((val * exp).round / exp)
-        ndigits == 0 ? val.to_i : val
-      end
-    end
   end
 
-  def si options = {}
+  def si(options = {})
     SI.convert(self, options)
   end
 
-  def si_byte length = 3
-    SI.convert(self, :length => length, :base => 1024, :min_exp => 0) + 'B'
+  def si_byte(length = 3)
+    SI.convert(self, length: length, base: 1024, min_exp: 0) + 'B'
   end
 end
-
